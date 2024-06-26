3f8888f1754cfec7b1d7ff428e0a9b7bb4651e03
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:41:02 +0500

    twentyone commit

---
+++ Assignment21/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// // Assignment 21
+Object.defineProperty(exports, "__esModule", { value: true });
+// they think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let objectcontainingItems = {
+    "Laptop": {
+        name: "Dell",
+        price: 40000,
+        quantity: 1,
+        author: "Dell",
+        Description: "High Performance Laptop with Fast Processor",
+    }
+};
+console.log(objectcontainingItems);
---
+++ Assignment21/main.ts
@@ -0,0 +1,20 @@
+// // Assignment 21
+
+// they think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let objectcontainingItems: {
+  [key: string]: any;
+} = {
+  "Laptop":{
+    name: "Dell",
+    price: 40000,
+    quantity: 1,
+    author: "Dell",
+    Description:"High Performance Laptop with Fast Processor",
+  }
+  
+  
+};
+
+console.log(objectcontainingItems);
+
