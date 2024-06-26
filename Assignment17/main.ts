a34e65c08eff99dc63a848b250cbe856354c9042
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:33:56 +0500

    seventeen commit

---
+++ Assignment17/main.ts
@@ -0,0 +1,38 @@
+// Assignment # 17
+
+
+// print message
+console.log("Unfortunately ! the new dinner table wont aarrive so we can invited only 2 guest" );
+
+
+let guestlist:string[]=["Usman","Umar", "Ali"];
+
+guestlist.unshift("Anas" ,"Omair");
+
+// Print message a Updted list
+console.log("Updated List of Guest",guestlist);
+
+// remove guest from the list
+
+while (guestlist.length>2) {
+    let removeguest:string|undefined=guestlist.pop();
+    if (removeguest !== undefined) {
+        console.log(`Sorry ${removeguest}, you cant invite them to dinner`);
+        
+        
+    }
+    
+}
+// • Print a message to each of the two people still on your list, letting them know they’re still invited.
+
+guestlist.forEach(guest =>{
+    console.log(`Dear ${guest}, you are both are you invited to dinner`);    
+}
+
+) 
+// removes 2 names from the list  
+guestlist.splice(0,guestlist.length);
+
+// print a message updatedemptylist
+
+console.log("updated list of guest",guestlist);
