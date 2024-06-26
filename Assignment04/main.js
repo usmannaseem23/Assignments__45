2fdd7ae2445cf64da38fb2280be7a31b347e852c
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:12:03 +0500

    fourth commit

---
+++ Assignment04/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Assignment # 4
+Object.defineProperty(exports, "__esModule", { value: true });
+// Storing The famous Quote and Author
+let famQuote = " “A person who never made a mistake never tried anything new.”";
+let Author = "Albert Einstein ";
+console.log(`${Author} once said,"${famQuote}"`);
---
+++ Assignment04/main.ts
@@ -0,0 +1,8 @@
+// Assignment # 4
+
+
+
+// Storing The famous Quote and Author
+let famQuote:string=" “A person who never made a mistake never tried anything new.”";
+let Author: string="Albert Einstein ";
+console.log(`${Author} once said,"${famQuote}"`);
