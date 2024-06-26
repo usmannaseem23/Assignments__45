08ca613a22304afa714b917a95da8f2914df24cf
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:07:57 +0500

    second  commit

---
+++ Assignment02/main.js
@@ -0,0 +1,6 @@
+//Assigment  # 1 
+//storinmng the person name in a variable 
+let personName = "Umar";
+//Printing a messge to the person
+console.log(`Hello ${personName} would you like to learn some Python today?`);
+export {};
---
+++ Assignment02/main.ts
@@ -0,0 +1,11 @@
+
+
+//Assigment  # 2
+
+//storinmng the person name in a variable 
+
+let personName:string = "Umar";
+
+//Printing a messge to the person
+
+console.log ( `Hello ${personName} would you like to learn some Python today?`)
