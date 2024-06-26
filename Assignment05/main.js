3e88b578591b42b387a50ede19d44642a19555ac
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:13:17 +0500

    fifth commit

---
+++ Assignment05/main.js
@@ -0,0 +1,6 @@
+// Assignment # 5
+let famousQuote = " “A person who never made a mistake never tried anything new.”";
+let famaousPerson = "Albert Einstein";
+let message = `${famaousPerson} once said,"${famousQuote}"`;
+console.log(message);
+export {};
---
+++ Assignment05/main.ts
@@ -0,0 +1,7 @@
+// Assignment # 5
+
+
+let famousQuote:string=" “A person who never made a mistake never tried anything new.”"
+let famaousPerson:string="Albert Einstein";
+let message:string=  `${famaousPerson} once said,"${famousQuote}"`;
+console.log(message);
