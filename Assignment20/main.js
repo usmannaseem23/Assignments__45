ff6b7746bc7c9cbe9783b9d5ac8910a762d267b1
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:39:55 +0500

    twenty commit

---
+++ Assignment20/main.js
@@ -0,0 +1,7 @@
+//Assignment # 20\
+let Countries = ["usa", "canada", "france", "germany", "japan"];
+// print list of countries
+console.log("List of Countries", Countries.forEach(country => {
+    console.log(country);
+}));
+export {};
---
+++ Assignment20/main.ts
@@ -0,0 +1,15 @@
+//Assignment # 20\
+
+
+
+let Countries:string[]=["usa","canada","france","germany","japan"];
+// print list of countries
+
+
+console.log("List of Countries" ,Countries.forEach(country =>{
+    console.log(country);
+})
+
+);
+
+
