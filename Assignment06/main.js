568d93ff9ddce0b7681dc67c4962296f80cca422
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:14:18 +0500

    six commit

---
+++ Assignment06/main.js
@@ -0,0 +1,6 @@
+// Assignment # 6
+let nameWithWhiteSpace = "\t \n umar naseem\t \n";
+console.log("name With White Space:", nameWithWhiteSpace);
+let strippedName = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+export {};
---
+++ Assignment06/main.ts
@@ -0,0 +1,11 @@
+// Assignment # 6
+
+
+let nameWithWhiteSpace:string="\t \n umar naseem\t \n";
+
+console.log( "name With White Space:" ,nameWithWhiteSpace);
+
+let strippedName:string=nameWithWhiteSpace.trim();
+console.log("stripped Name:",strippedName);
+
+
