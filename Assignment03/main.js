ede2707050c89217924e15640803d064490d29dc
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:08:53 +0500

    third commit

---
+++ Assignment03/main.js
@@ -0,0 +1,6 @@
+//Assignment 3
+// Storing The famous Quote and Author
+let famQuote = " “A person who never made a mistake never tried anything new.”";
+let Author = "Albert Einstein ";
+console.log(`${Author} once said,${famQuote}`);
+export {};
---
+++ Assignment03/main.ts
@@ -0,0 +1,11 @@
+// Assignment 3
+
+
+
+// Storing a Varaible person'name
+let personName: string ="usman";
+// 
+console.log(personName.toUpperCase());
+console.log(personName.toLowerCase());
+console.log(personName.replace(/\b\w/g,(char)=>char.toUpperCase()));
+ 
