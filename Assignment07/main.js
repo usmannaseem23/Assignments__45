a5d193e8bd379d4412e7af1d77f8473a1200f005
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:20:19 +0500

    seven commit

---
+++ Assignment07/main.js
@@ -0,0 +1,10 @@
+// Assignment # 7
+let additionResult = 5 + 3;
+console.log("Addition Result", additionResult);
+let subtractionResult = 10 - 2;
+console.log("Subtraction Result", subtractionResult);
+let multiplicationResult = 4 * 2;
+console.log("Multiplication Result", multiplicationResult);
+let divisionResult = 16 / 2;
+console.log("Division Result", divisionResult);
+export {};
---
+++ Assignment07/main.ts
@@ -0,0 +1,18 @@
+// Assignment # 7
+
+
+let additionResult:number=5+3
+console.log("Addition Result" ,additionResult);
+
+let subtractionResult:number=10-2;
+
+console.log("Subtraction Result" ,subtractionResult);
+
+let multiplicationResult:number=4*2;
+
+console.log("Multiplication Result" ,multiplicationResult);
+
+let divisionResult:number=16/2;
+
+console.log("Division Result" ,divisionResult);
+
