5e4607d12acbb5188b0e7062bed9655ead482b7a
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:25:14 +0500

    eleven commit

---
+++ Assignment11/main.js
@@ -0,0 +1,17 @@
+// Assignment # 11
+let names = ["Usman", "Umar", "Shahab", "Omair", "Osama"];
+// loop
+// for each
+// for loop
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+}
+// for each
+names.forEach(name => {
+    console.log(name);
+});
+// for of loop
+for (const iterator of names) {
+    console.log(iterator);
+}
+export {};
---
+++ Assignment11/main.ts
@@ -0,0 +1,38 @@
+// Assignment # 11
+
+
+let names:string[]=["Usman","Umar","Shahab" ,"Omair","Osama"];
+
+// loop
+// for each
+// for loop
+
+for (let index = 0; index < names.length; index++) {
+    console.log(names
+    [index]);
+    
+}
+    
+
+
+// for each
+names.forEach(name => {
+    console.log(name);
+    
+});
+
+// for of loop
+for (const iterator of names) {
+    console.log(iterator);
+    
+}
+
+
+
+
+
+
+
+
+
+
