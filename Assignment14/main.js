37312e18899615c0027c39baa8d68ab643fcbb73
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:30:40 +0500

    fourteen commit

---
+++ Assignment14/main.js
@@ -0,0 +1,7 @@
+// Assignment # 14
+let guestList = ["Usman", "Umar", "Osama"];
+// Invite each for dinner
+guestList.forEach(guest => {
+    console.log(`Dear ${guest} , You are cordially invited to dinner`);
+});
+export {};
---
+++ Assignment14/main.ts
@@ -0,0 +1,12 @@
+// Assignment # 14
+
+
+let guestList:string[]=["Usman","Umar","Osama"];
+
+// Invite each for dinner
+
+guestList.forEach(guest => {
+
+    console.log(`Dear ${guest} , You are cordially invited to dinner` );
+     
+});
