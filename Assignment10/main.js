29323c6831595e9ba429ff5c379be216972dbd38
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:24:14 +0500

    ten commit

---
+++ Assignment10/main.js
@@ -0,0 +1,9 @@
+// Assignment # 10
+// This Program adds two numbers and Print The Result
+let number1 = 80;
+let number2 = 29;
+// Add New numbers
+let result = number1 + number2;
+// Print Result
+console.log(result);
+export {};
---
+++ Assignment10/main.ts
@@ -0,0 +1,10 @@
+// Assignment # 10
+
+// This Program adds two numbers and Print The Result
+let number1:number=80;
+
+let number2:number=29;
+// Add New numbers
+let result:number=number1+number2;
+// Print Result
+console.log(result);
