c444433d5e73013db9a650057aaeaaee9ad9dd23
usman224480 <usmannaseem2005@gmail.com>
2024-06-26 23:22:40 +0500

    nine commit

---
+++ Assignment09/main.js
@@ -0,0 +1,5 @@
+// Assignment # 9
+let favouritenumber = 2;
+let message = `My Favourite number:${favouritenumber}`;
+console.log(message);
+export {};
---
+++ Assignment09/main.ts
@@ -0,0 +1,8 @@
+// Assignment # 9
+
+
+let favouritenumber:number=2;
+
+let message:string=`My Favourite number:${favouritenumber}`;
+
+console.log(message);
