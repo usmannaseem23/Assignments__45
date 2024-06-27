11977021a891ce97b90c7d36e458caafe9376deb
usman224480 <usmannaseem2005@gmail.com>
2024-06-27 15:36:52 +0500

    fortyone commit

---
+++ Assignment41/main.js
@@ -0,0 +1,9 @@
+// Assignment # 41
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), 
+// which prints the name of each magician in the array.
+function show_magicians(magicians) {
+    magicians.forEach(name => console.log(name));
+}
+let magicians_names = ["Usman", "Ali", "Umar"];
+show_magicians(magicians_names);
+export {};
