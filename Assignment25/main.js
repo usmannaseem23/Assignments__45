6345b65c0fb507bffc5ae8530f3a985b8e258f9f
usman224480 <usmannaseem2005@gmail.com>
2024-06-27 15:18:21 +0500

    twentyfive

---
+++ Assignment25/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Assignment # 25
+Object.defineProperty(exports, "__esModule", { value: true });
+// Alien Colors #1: Imagine an alien was just shot down in a game.
+//  Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'
+let aliencolor = "green";
+if (aliencolor === "green") {
+    console.log("Alien colir is green player just earned 5 Points");
+}
+;
+if (aliencolor === "yellow") {
+    console.log("Alien color is yellow");
+}
+;
---
+++ Assignment25/main.ts
@@ -0,0 +1,17 @@
+//Assignment # 25
+
+// Alien Colors #1: Imagine an alien was just shot down in a game.
+//  Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'
+
+let aliencolor = "green";
+
+if (aliencolor === "green") {
+    console.log("Alien colir is green player just earned 5 Points");
+    
+    
+};
+
+if (aliencolor === "yellow") {
+    console.log("Alien color is yellow");
+    
+};
