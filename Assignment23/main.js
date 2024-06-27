9c47cce8dcf1fdf0c0dd7e8165fdb59f9663510b
usman224480 <usmannaseem2005@gmail.com>
2024-06-27 15:15:13 +0500

    twentythree

---
+++ Assignment23/main.js
@@ -0,0 +1,34 @@
+// Assignment # 23
+// Conditional Tests: Write a series of conditional 10 tests.
+// Test # 1
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test # 2
+console.log("Is car =='toyoto'? I predict False.");
+console.log(car == 'toyoto');
+// Test # 3
+console.log("Its Car !=  `subaru`? I predict False ");
+console.log(car != 'subaru');
+// Test # 4
+console.log("Its Car ===  `subaru`? I predict False");
+console.log(car === 'subaru');
+// Test # 5
+console.log("Is car.length > 0 ? I Predict True");
+console.log(car.length > 0);
+// Test # 6
+console.log("Is car.length < 5 ? I Predict False");
+console.log(car.length < 5);
+// Test # 7
+console.log("Is car.toUppercase() === `SUBARU` ? I Predict True");
+console.log(car.toUpperCase() === 'SUBARU');
+// Test # 8
+console.log("Is car.toUppercase() === `subaru` ? I Predict False");
+console.log(car.toUpperCase() === 'subaru');
+// Test # 9
+console.log("Is car.toLowerCase() === `subaru` ? I Predict True");
+console.log(car.toLowerCase() === 'subaru');
+// Test # 10
+console.log("Is car.toLowerCase() === `SUBARU` ? I Predict False");
+console.log(car.toLowerCase() === 'SUBARU');
+export {};
