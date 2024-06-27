b42b64a59b5f51e2b8fbabd153952c2ba33fa519
usman224480 <usmannaseem2005@gmail.com>
2024-06-27 15:33:40 +0500

    thirtynine commit

---
+++ Assignment39/main.ts
@@ -0,0 +1,19 @@
+// Assignment # 39
+
+// City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+
+// "Lahore, Pakistan"
+
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+
+function city_Country(city:string , country:string) :string {
+    return `${city} , ${country}`
+    
+}
+// calling a function and print the returned value
+console.log(city_Country("Lahore" , "Pakistan"));
+
+ console.log(city_Country("Tokyo" ,"Japan"));
+
+ console.log(city_Country("Berlin" ,"Germany"));
+
