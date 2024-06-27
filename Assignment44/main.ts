f55bfadc7bcd1530fa2973408b3e1e4673c8225a
usman224480 <usmannaseem2005@gmail.com>
2024-06-27 15:39:56 +0500

    fortyfour commit

---
+++ Assignment44/main.ts
@@ -0,0 +1,23 @@
+// Assignment # 44
+
+
+// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. 
+// The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered.
+//  Call the function three times, using a different number of arguments each time.
+
+function makeSandwich(...items:string[]) {
+    console.log("\nMaking a sandwich with the following items:\n");
+
+    items.forEach(singleItems => console.log("-" +singleItems));
+
+    console.log("\nNow Enjoy Sandwich");
+    
+    
+    
+}
+// Calling the function 3 times with 3 different number of arguments
+makeSandwich("Chicken" , "Cheese" ,"Mayo" ,"Egg"); 
+
+makeSandwich("Bread" ,"Butter");
+
+makeSandwich("Bread" ,"Butter" ,"Mayo" ,"Egg", "Cheese" ,"Chicken" ,"Lettuce","Tomato");
